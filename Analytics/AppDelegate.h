6c8313e8a928941b91695cc27fbb6537492e251e
Maximilian Tagher <feedback.tagher@gmail.com>
2012-12-01 10:02:07 -0800

    Initial Commit

---
+++ Analytics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Analytics
+//
+//  Created by Maximilian Tagher on 12/1/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Analytics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Analytics
+//
+//  Created by Maximilian Tagher on 12/1/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
